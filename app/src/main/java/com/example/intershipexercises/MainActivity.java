f395a3a2be4823ca6273c0dbbb43f84cd068be7a
Lazar Alexandru <lazar.alexandru97@gmail.com>
2020-03-09 12:43:54 +0200

    savedInstance-on going

--- app/src/main/java/com/example/intershipexercises/MainActivity.java
+++ app/src/main/java/com/example/intershipexercises/MainActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+    private int increment_value = 0;
+    private TextView incrementTv;
+    private Button incrementBtn;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Happy to be born");
+        initViews();
+        int map_key_counter = savedInstanceState.getInt("myCounter");
+        if( (savedInstanceState!= null) ){
+            increment_value = map_key_counter;
+        }
     }
 
     @Override
@@ -21,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Log.d(TAG, "onStart: I exist, but you can;t see me");
     }
+
+    private void initViews(){
+        incrementTv = findViewById(R.id.counter_value_tv);
+        incrementBtn = findViewById(R.id.increment_button);
+        incrementTv.setText(increment_value + "");
+        incrementBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increment_value++;
+                incrementTv.setText(increment_value + "");
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("myCounter", increment_value);
+    }
+
+
 
     @Override
     protected void onResume() {
